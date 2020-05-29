0866e40dddb329c31fc5da7fd74a0af9ec991ca0
Rantoniaina <jaona.rantoniaina@gmail.com>
2020-05-29 11:45:40 +0300

    Google sign in authentification 3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/Firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './Header.styles.scss';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -16,6 +17,15 @@ const Header = () => (
       <Link to="/shop" className="option">
         CONTACT
       </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SING IN
+        </Link>
+      )}
     </div>
   </div>
 );
