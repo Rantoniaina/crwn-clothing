caaa662a143de2ec9406109ba397c894c506bea1
Rantoniaina <jaona.rantoniaina@gmail.com>
2020-05-30 11:51:31 +0300

    Connect and mapStateToProps

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/Firebase.utils';
-
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './Header.styles.scss';
+import { connect } from 'react-redux';
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -30,4 +30,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
