2147982b7ef326b57d436448d075ed577288d252
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2020-11-04 21:32:57 +0300

    WithSpinner HOC

--- src/redux/Shop/Shop.reducer.js
+++ src/redux/Shop/Shop.reducer.js
@@ -1,8 +1,7 @@
-import SHOP_DATA from "./Shop.data";
 import ShopActionsTypes from "./Shop.types";
 
 const INITIAL_STATE = {
-  collections: SHOP_DATA,
+  collections: null,
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
