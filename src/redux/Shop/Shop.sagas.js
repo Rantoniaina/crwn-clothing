88752a8a9b6ea9815ffb7ff7d3ac3d2ec6a0a5f4
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2020-11-18 21:27:45 +0300

    Redux Thunk Into Saga

--- src/redux/Shop/Shop.sagas.js
+++ src/redux/Shop/Shop.sagas.js
@@ -1,8 +1,27 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, call, put } from "redux-saga/effects";
 import ShopActionsTypes from "./Shop.types";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/Firebase.utils";
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from "./Shop.actions";
 
 export function* fetchCollectionsAsync() {
   yield console.log("I am fired");
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
