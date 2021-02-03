c87ca4f2c5f08b9bd5d55cb91854aa1b24d8f4f0
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2021-02-03 22:21:06 +0300

    Deploying to production

--- client/src/components/StripeButton/StripeButton.jsx
+++ client/src/components/StripeButton/StripeButton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ const StripeCheckoutButton = ({ price }) => {
     "pk_test_51HfV3lIdpRmYKLLFhYvMIKZbV0DpuFtvSouM0ffDynh1TS3vYky7q88nn0rMODR8ueh23GOF3c0soOjpB5PjUecg00HpsGaxme";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "POST",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please make sure you use the provided credit card."
+        );
+      });
   };
 
   return (
