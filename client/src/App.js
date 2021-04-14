f5deac66f598778ac527780aafbb4069d73f40c8
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2021-04-14 11:26:39 +0300

    Mobile responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import "./App.css";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Shop from "./pages/Shop/Shop";
 import HomePage from "./pages/HomePage/HomePage";
@@ -10,6 +9,7 @@ import { selectCurrentUser } from "./redux/User/User.selectors";
 import { createStructuredSelector } from "reselect";
 import Checkout from "./pages/Checkout/Checkout";
 import { checkUserSession } from "./redux/User/User.actions";
+import { GlobalStyle } from "./global.styles";
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
@@ -18,6 +18,7 @@ const App = ({ checkUserSession, currentUser }) => {
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
