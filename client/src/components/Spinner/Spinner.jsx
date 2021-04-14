246ffe7b30723c8b8662687b94a7f5fb56a60154
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2021-04-14 15:46:42 +0300

    React lazy + Suspense

---
+++ client/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SpinnerContainer, SpinnerOverlay } from './styles.js';
+
+const Spinner = () => (
+    <SpinnerOverlay>
+        <SpinnerContainer />
+    </SpinnerOverlay>
+);
+
+export default Spinner;
